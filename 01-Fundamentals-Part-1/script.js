5d2329af3ef8f6d53cf4d61b84d36f186c1c4cd5
Robert Paech <robert.paech@gmail.com>
2023-07-14 11:56:16 +1000

    Section 30 complete.

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,23 +1,28 @@
-// let js = true;
-// console.log(40 + 8 + 23 - 10);
 
-// let firstName = "Robert";
+let js = true;
+console.log(40 + 8 + 23 - 10);
 
-// console.log(firstName);
+let firstName = "Robert";
 
-// const PI = 3.1415;
+console.log(firstName);
+
+const PI = 3.1415;
 // PI += 1;
 
-// let firstString = "Robert's";
-// let secondString = 'Here we are using "quotes" in our string';
+let firstString = "Robert's";
+let secondString = 'Here we are using "quotes" in our string';
 
-let massMark = 78;
-let heightMark = 1.69;
-let massJohn = 92;
-let heightJohn = 1.95;
+
+// challenge 1
+
+/*
+let massMark = 95;
+let heightMark = 1.88;
+let massJohn = 85;
+let heightJohn = 1.76;
 
 function calcBMI (mass, height) {
-    return (mass / height * height);
+    return mass / (height * height);
 }
 
 let BMIMark = calcBMI(massMark, heightMark);
@@ -25,3 +30,30 @@ let BMIJohn = calcBMI(massJohn, heightJohn);
 let markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+
+let templateLiteral = `I'm ${firstName}`;
+console.log(templateLiteral);
+
+let multiLineString = 
+`This is a string that
+spans multiple lines.
+I wonder if the tab is
+retained?`;
+
+console.log(multiLineString);
+
+if (js) {
+    console.log(`foo`);
+} else {
+    console.log('bar');
+}
+
+const val1 = 18;
+const val2 = 22;
+const str1 = "foo";
+const str2 = "bar";
+
+if (val1 == val2) console.log(true);
+
+typeof("foo");
