35031b01a666e3ccb9b0d1a712a69df6042a2199
Robert Paech <robert.paech@gmail.com>
2023-07-14 10:37:06 +1000

    challenge-1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,12 +1,27 @@
-let js = true;
-console.log(40 + 8 + 23 - 10);
+// let js = true;
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = "Robert";
+// let firstName = "Robert";
 
-console.log(firstName);
+// console.log(firstName);
 
-const PI = 3.1415;
-PI += 1;
+// const PI = 3.1415;
+// PI += 1;
 
-let firstString = "Robert's";
-let secondString = 'Here we are using "quotes" in our string';
+// let firstString = "Robert's";
+// let secondString = 'Here we are using "quotes" in our string';
+
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+function calcBMI (mass, height) {
+    return (mass / height * height);
+}
+
+let BMIMark = calcBMI(massMark, heightMark);
+let BMIJohn = calcBMI(massJohn, heightJohn);
+let markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
