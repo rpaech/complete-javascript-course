aca1c9457d98d8f79f3cfe93b80727bc972cfbea
Robert Paech <robert.paech@gmail.com>
2023-07-14 10:04:18 +1000

    Completed section 2 part 14.

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,12 @@
+let js = true;
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Robert";
+
+console.log(firstName);
+
+const PI = 3.1415;
+PI += 1;
+
+let firstString = "Robert's";
+let secondString = 'Here we are using "quotes" in our string';
